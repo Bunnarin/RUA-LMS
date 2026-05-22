45c09f43b1f4a488e80badc9a827792c838360e7
Bunnarin Chrun <chrunbunnarin857@gmail.com>
2026-05-22 08:30:12 +0700

    now seggregate schedule by sem

--- storage/hooks/event/onScheduleList.ts
+++ storage/hooks/event/onScheduleList.ts
@@ -14,31 +14,29 @@ export default class OnScheduleListPlugin extends Plugin {
       const semesters = await this.db.getRepository('semester').find({
         filter: {
             $or: [
-                { startDate: { $dateOn: { type: "lastYear" } } },
-                { startDate: { $dateOn: { type: "thisYear" } } },
-                { startDate: { $dateOn: { type: "nextYear" } } }
-              ]
+              { startDate: { $dateOn: { type: "lastYear" } } },
+              { startDate: { $dateOn: { type: "thisYear" } } },
+              { startDate: { $dateOn: { type: "nextYear" } } }
+            ]
           }
       });
 
       // find the semester whose end is closest to now
-      const semester = semesters.reduce((prev: any, curr: any) => {
+      const prevSemester = semesters.reduce((prev: any, curr: any) => {
           const time = (dateStr: string) => new Date(dateStr).getTime();
           return time(curr.get('endDate')) < time(prev.get('endDate')) ? curr : prev;
       });
 
-      const currentSchedules: any[] = [];
-      const nonCurrentSchedules: any[] = [];
+      // the current sem will be the next one
+      const currentSemester = semesters.find((s: any) => s.get('id') > prevSemester.get('id'));
 
-      const isCurrent = (s: any) =>
-        s.get('course')?.get('semesterNumber') === semester.get('number');
-      
-      for (const schedule of ctx.body.rows) {
-        if (isCurrent(schedule))
-          currentSchedules.push(schedule);
-        else
-          nonCurrentSchedules.push(schedule);
-      }
+      let schedules = ctx.body.rows;
+
+      // we filter by sem based on tab cuz we can't do inherited block template on the UI
+      if (ctx.get('referer')?.endsWith('/tab/72u1j3zz86f'))
+        schedules = schedules.filter((s: any) => s.get('course')?.get('semesterNum') === currentSemester.get('number'));
+      else if (ctx.get('referer')?.endsWith('/tab/f7cgx8q2xbf'))
+        schedules = schedules.filter((s: any) => s.get('course')?.get('semesterNum') !== currentSemester.get('number'));
 
       const isLecturer = (s: any) =>
         (s.get('lecturers') ?? []).some((l: any) => l.id === userId);
@@ -48,7 +46,7 @@ export default class OnScheduleListPlugin extends Plugin {
       const lecturerSchedules: any[] = [];
       const coLecturerSchedules: any[] = [];
       const rest: any[] = [];
-      for (const schedule of currentSchedules) {
+      for (const schedule of schedules) {
         if (isLecturer(schedule))
           lecturerSchedules.push(schedule);
         else if (isCoLecturer(schedule))
@@ -57,7 +55,7 @@ export default class OnScheduleListPlugin extends Plugin {
           rest.push(schedule);
       }
 
-      ctx.body.rows = [...lecturerSchedules, ...coLecturerSchedules, ...rest, ...nonCurrentSchedules];
+      ctx.body.rows = [...lecturerSchedules, ...coLecturerSchedules, ...rest];
     });
   }
 }
